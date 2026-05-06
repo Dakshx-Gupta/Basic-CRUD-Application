ab932a8b6062144f35e9048201ac57f36a11e847
Daksh Gupta <139779998+Dakshx-Gupta@users.noreply.github.com>
2026-05-06 19:56:56 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,11 @@ if(isset($_GET['id'])){
     include("connect.php");
     $sql = "DELETE FROM data_library WHERE id = $id";
     if(mysqli_query($conn, $sql)){
+        // Renumber all IDs sequentially
+        mysqli_query($conn, "SET @count = 0");
+        mysqli_query($conn, "UPDATE data_library SET id = (@count := @count + 1)");
+        mysqli_query($conn, "ALTER TABLE data_library AUTO_INCREMENT = 1");
+
         session_start();
         $_SESSION['delete'] = "Book removed Successfully";
         header("Location:index.php");
